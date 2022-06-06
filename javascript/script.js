5c941081e12e7404159bf2e991e5744e77ecdbe3
venkateshvanaboina <venkateshvanaboina95531998@gmail.com>
2022-06-06 12:47:11 +0530

    portfolio created

---
+++ javascript/script.js
@@ -0,0 +1,198 @@
+// document.getElementById("para").innerHTML =" hello"
+// console.log(x);
+// document.write("hello");
+
+// document.getElementById("para").innerHTML ="Namastha JAvaScript"
+
+// var x;
+// var y;
+// x=10;
+// y=23;
+// console.log(x*y);
+
+// document.write("Hello world")
+
+// function divisionNumber(x,y){
+//     return x / y;
+// }
+
+//     let z = divisionNumber(4,2);
+
+// function myFunction(x,y) {
+//     return x + y;
+//     // console.log("hello");
+// }
+
+// console.log(myFunction (12,3));
+// ................................................................
+// .............Function Expression.........
+// const myArea = function(x,y) {
+//     return x * y;
+// }
+// console.log(myArea(12,12))
+// ...............................................................
+
+// local and gloabal varaible
+
+// ...local varaible
+// var lastname = "Venkatesh";
+
+// function firstname() {
+//     lastname="vanaboina" //overrided the above value
+//     console.log(lastname);
+// }
+
+// firstname();
+
+
+
+// let latname = true;
+
+// function bolle() {
+//     latname=false;  //overrided the above value
+//     console.log(latname);
+// }
+
+// bolle();
+
+// console.log(latname);
+
+
+// const late = 123;
+
+// function numbe() {
+//     late= 3212;  //overrided the above value but for const it will show error
+//     console.log(late);
+// }
+
+// numbe();
+
+//Global VAraibles
+
+// function printName() {
+//     let firstname = "Venkatesh"
+//     console.log(firstname);
+// }
+
+// printName();
+
+// console.log(firstname);  //outside the function there will be not value assign for these firstname
+
+// let firname = "Vanbaoina";
+
+// function priName() {
+//     firname = "Venkatesh"
+//     console.log(firname);
+// }
+
+// priName();
+
+// console.log(firname);
+
+
+//****************OBJECT******************  (Important topic)
+
+
+// var student = {
+//     roll_no: 24,
+//     firstname: 'Venkatesh',
+//     lastname: 'Vanaboina',
+//     class: 10,
+//     section: 4,
+//     isPassed: false,
+//     fullname: function(){
+//         return this.class +' '+ this.firstname +' '+ this.lastname
+//     }
+// }
+
+// //console.log(student.fullname());
+// //console.log(student['lastname']);
+// console.log(student.firstname +' '+  student.lastname )
+
+// var lastfirst = "Venkatesh Vanaboina"
+// //automaticall the javascript includes the options for the string object like length etc..................
+// console.log(lastfirst.length);
+
+//************************************ARRAY***************************** */  (Important topic)
+// let arr =["Ap","ts",'up','er',1,2,4];
+
+// console.log(arr);
+
+// let firstName = "Wdbla@Development@Ram@Test"
+
+// let result = firstName.split("@");
+
+// console.log(result[1]);
+
+
+// var student = {
+//     firstName: 'venkatesh',
+//     lastName: 'vanaboina',
+//     section: 4,
+//     class:9,
+//     ispaseed: true,
+//     fullName: function () {
+//         return this.firstName +' '+ this.lastName;
+//     },
+//     sectionClass: function (){
+//         return this.section +' '+this.class;
+//     }
+// }
+
+// console.log(student.fullName());
+// console.log(student.sectionClass());
+
+// function myFunction (a,b){
+//     return a*b;
+// }
+// console.log(myFunction(10,2));
+
+
+// function printName(){
+//     console.log("Hello Venky");
+
+// }
+// printName();
+
+
+//...............................operators.......................
+
+// let x=5;
+
+// console.log(x =="5");//true or false (== it will compare value)
+
+// console.log(x != "4");
+
+// let x =5;
+// console.log(x > 10);
+// console.log(x < 10);
+// console.log(x <= "10");
+// console.log(x >= 10);
+
+
+// conditions...........................
+// if(){
+//statements
+// }
+
+let x = 10;
+let y = 5;
+let z = 1;
+//&& and symbol  .....  || or symbol
+// if(x != y &&  x != z || x<z){
+//     console.log("statement is true ");
+// }
+// else {
+//     console.log("statement is false")
+// }
+
+// laddeer condition
+if (x>y) {
+ } else if (x>y) {
+
+    }else if() {
+
+    }else if () {
+
+    }
+    
